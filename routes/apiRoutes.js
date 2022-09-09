dcd8514f272ebe18a297a4682ecd9a99d44767db
Ajalloh1 <mys4jalloh@gmail.com>
2022-09-09 18:02:22 -0400

    fixed my routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,41 +1,44 @@
 const router = require('express').Router();
-const dbNote = require('../db/db.json');
+// const dbNote = require('../db/db.json');
+const fs = require('fs');
+const path = require('path');
+const { v1: uuidv1 } = require('uuid');
 
 // calling of api functions//
-router.get('/api/notes', (req, res) => {
-    fs.readFile(__dirname + "/db/db.json", 'utf8', function (error, data) {
+router.get('/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, "../db/db.json"), 'utf8', function (error, data) {
         if (error) {
-          return console.log(error)
+            return console.log(error)
         }
         console.log("This is Notes", data)
         res.json(JSON.parse(data))
     })
 });
 
-router.post('/api/notes', (req, res) => { 
+router.post('/notes', (req, res) => {
     const { title, text } = req.body;
 
-    fs.readFile(__dirname + "/db/db.json", 'utf-8', (err, data) => {
+    fs.readFile(path.join(__dirname, "../db/db.json"), 'utf-8', (err, data) => {
         if (err) {
             throw err;
         }
 
         // This parses the json file to a string
-        
+
         const notes = JSON.parse(data.toString());
 
         notes.push({
-            "id": uuid(),
-            title, 
+            id: uuidv1(),
+            title,
             text
         });
-        
+
         // This will log the json object
         console.log(notes);
-        
-        fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), function (error, data) {
+
+        fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(notes), function (error, data) {
             if (error) {
-              return error
+                return error
             }
             console.log(notes)
             res.json(notes);
@@ -44,8 +47,8 @@ router.post('/api/notes', (req, res) => {
     });
 });
 
-router.delete("/api/notes/:id", function (req, res) {
-    
+router.delete("/notes/:id", function (req, res) {
+
     const noteId = req.params.id
 
     fs.readFile(__dirname + "/db/db.json", 'utf-8', (err, data) => {
@@ -62,7 +65,7 @@ router.delete("/api/notes/:id", function (req, res) {
 
         fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), function (error, data) {
             if (error) {
-              return error
+                return error
             }
             console.log(notes)
             res.json(notes);
